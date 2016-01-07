7d44fdecdd896075668b4bbd0100a3baa7875ca6
revesansparole <revesansparole@gmail.com>
2016-01-07 17:10:55 +0100

    site working

--- seeweb/views/main.py
+++ seeweb/views/main.py
@@ -3,5 +3,5 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/index.jinja2')
 def index(request):
-    # del request
+    del request
     return {'project': 'seeweb'}

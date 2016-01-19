1e0999afa6398146389d74bdef155487c15c890f
revesansparole <revesansparole@gmail.com>
2016-01-19 10:00:53 +0100

    added role in listing projects

--- seeweb/views/project/listing.py
+++ seeweb/views/project/listing.py
@@ -1,14 +1,18 @@
 from pyramid.view import view_config
 
 from seeweb.models import DBSession
+from seeweb.models.access import project_access_role
 from seeweb.models.auth import Role
 from seeweb.models.project import Project
+from seeweb.views.tools import get_current_uid
 
 
 @view_config(route_name='project_list',
              renderer='templates/project/listing.jinja2')
 def view(request):
     session = DBSession()
+    current_uid = get_current_uid(request)
+
     query = session.query(Project)
     if 'query' in request.params and "all" not in request.params:
         search_pattern = "%s%%" % request.params['query']
@@ -17,7 +21,11 @@ def view(request):
         search_pattern = ""
 
     query = query.order_by(Project.id)
-    projects = [(Role.read, project) for project in query.all()]
+    projects = []
+    for project in query.all():
+        role = project_access_role(session, project, current_uid)
+        if role != Role.denied:
+            projects.append((role, project))
 
     return {'projects': projects,
             'search_pattern': search_pattern}

25bc45450d46596877c83efb1c8814b94ccffb67
revesansparole <revesansparole@gmail.com>
2016-02-19 08:31:54 +0100

    scaffold for known project content item categories

---
+++ seeweb/project/content/interface/views/view_item.py
@@ -0,0 +1,13 @@
+from pyramid.view import view_config
+
+from seeweb.models import DBSession
+from seeweb.views.project.commons import content_init
+
+
+@view_config(route_name='project_content_interface_view_item',
+             renderer='../templates/view_item.jinja2')
+def view(request):
+    session = DBSession()
+    project, interface, view_params = content_init(request, session)
+
+    return view_params
--- seeweb/scripts/initializedb.py
+++ seeweb/scripts/initializedb.py
@@ -217,10 +217,10 @@ This project is part of OpenAlea_.
 
         notebook = Project.create(session, 'revesansparole', 'notebook_pjt')
         notebook.public = True
-        # for i in range(5):
-        #     item = ContentItem.create(session, uuid1().hex, "executable",
-        #                               notebook)
-        #     item.name = "executable%d" % i
+        for i in range(5):
+            item = ContentItem.create(session, uuid1().hex, "executable",
+                                      notebook)
+            item.name = "executable%d" % i
 
         for i in range(5):
             item = ContentItem.create(session, uuid1().hex, "notebook",
@@ -231,49 +231,59 @@ This project is part of OpenAlea_.
         nodelib = Project.create(session, 'revesansparole', 'nodelib')
         nodelib.public = True
 
-        # Interface.create(session, nodelib, "IInt")
-        # Interface.create(session, nodelib, "IStr")
+        item = ContentItem.create(session, uuid1().hex, "interface", nodelib)
+        item.name = "IInt"
+        item = ContentItem.create(session, uuid1().hex, "interface", nodelib)
+        item.name = "IStr"
 
-        # ndefs = []
-        # for i in range(3):
-        #     node_def = dict(id=uuid1().hex,
-        #                     name="read%d" % i,
-        #                     category="oanode",
-        #                     description="toto was here",
-        #                     author="revesansparole",
-        #                     function="testio:read",
-        #                     inputs=[dict(name="in1", interface="IInt",
-        #                                  value="0", descr="counter"),
-        #                             dict(name="in2", interface="IStr",
-        #                                  value="a", descr="unit")],
-        #                     outputs=[dict(name="ret", interface="IInt",
-        #                                   descr="important result")])
-        #
-        #     WorkflowNode.create(session, nodelib, node_def)
-        #     ndefs.append(node_def)
+        ndefs = []
+        for i in range(3):
+            node_def = dict(id=uuid1().hex,
+                            name="read%d" % i,
+                            category="oanode",
+                            description="toto was here",
+                            author="revesansparole",
+                            function="testio:read",
+                            inputs=[dict(name="in1", interface="IInt",
+                                         value="0", descr="counter"),
+                                    dict(name="in2", interface="IStr",
+                                         value="a", descr="unit")],
+                            outputs=[dict(name="ret", interface="IInt",
+                                          descr="important result")])
+
+            item = ContentItem.create(session, node_def['id'],
+                                      "workflow_node", nodelib)
+            item.name = node_def['name']
+            item.author = node_def['author']
+            item.store_definition(node_def)
+            ndefs.append(node_def)
 
         workflow = Project.create(session, 'revesansparole', 'workflow')
         workflow.public = True
         workflow.add_auth(session, oa, Role.view)
 
-        # workflow_def = dict(id=uuid1().hex,
-        #                     name="sample_workflow",
-        #                     category="oaworkflow",
-        #                     description="trying some stuff",
-        #                     author="revesansparole",
-        #                     nodes=[dict(id=ndefs[0]['id'], label="node1",
-        #                                 x=100, y=10),
-        #                            dict(id=ndefs[1]['id'], label=None,
-        #                                 x=200, y=10),
-        #                            dict(id=ndefs[2]['id'], label=None,
-        #                                 x=150, y=100),
-        #                            dict(id=uuid1().hex, label="fail",
-        #                                 x=150, y=200)],
-        #                     connections=[(0, "ret", 2, "in1"),
-        #                                  (1, "ret", 2, "in2"),
-        #                                  (2, "ret", 3, "in")])
-        #
-        # Workflow.create(session, workflow, workflow_def)
+        workflow_def = dict(id=uuid1().hex,
+                            name="sample_workflow",
+                            category="oaworkflow",
+                            description="trying some stuff",
+                            author="revesansparole",
+                            nodes=[dict(id=ndefs[0]['id'], label="node1",
+                                        x=100, y=10),
+                                   dict(id=ndefs[1]['id'], label=None,
+                                        x=200, y=10),
+                                   dict(id=ndefs[2]['id'], label=None,
+                                        x=150, y=100),
+                                   dict(id=uuid1().hex, label="fail",
+                                        x=150, y=200)],
+                            connections=[(0, "ret", 2, "in1"),
+                                         (1, "ret", 2, "in2"),
+                                         (2, "ret", 3, "in")])
+
+        item = ContentItem.create(session, workflow_def['id'],
+                                  "workflow", workflow)
+        item.name = workflow_def['name']
+        item.author = workflow_def['author']
+        item.store_definition(workflow_def)
 
         # spl = Project.create(session, 'revesansparole', 'sample_project')
         # spl.public = True

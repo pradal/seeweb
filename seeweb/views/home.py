549cee5443351cb4e9f978b1ef86b1463da3fced
revesansparole <revesansparole@gmail.com>
2016-02-26 12:07:09 +0100

    save back route in home

--- seeweb/views/home.py
+++ seeweb/views/home.py
@@ -9,6 +9,7 @@ from.commons import view_init
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def view(request):
+    request.session['last'] = request.current_route_url()
     session = DBSession()
     view_params = view_init(request, session)
 

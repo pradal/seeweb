8257d647f69753516ebb2395ec5c2cd0c3f9d9dc
revesansparole <revesansparole@gmail.com>
2016-02-26 16:14:28 +0100

    cleanup

--- seeweb/project/content/notebook/views/view_item.py
+++ seeweb/project/content/notebook/views/view_item.py
@@ -11,34 +11,11 @@ def view(request):
     session = DBSession()
     project, notebook, nbdef, view_params = content_init(request, session)
 
-    notebook_cells = []
-    if nbdef is not None:
+    if nbdef is None:
+        notebook_cells = []
+    else:
         nbdef = nbformat.convert(nbformat.from_dict(nbdef), 4)
-        notebook_cells.extend(nbdef.cells)
-        # # nbdef = nbformat.reads(notebook.definition, 4)
-        #
-        # # if 'cells' not in nbdef:  # TODO workaround
-        # #     nbdef = nbdef['worksheets'][0]
-        #
-        # for cell in nbdef.cells:
-        #     src = [v.strip() for v in cell.get('source', cell.get("input"))]
-        #     res = []
-        #     for out in cell.outputs:
-        #         if out.output_type == "stream":
-        #             res.append(('stream', out.text))
-        #         elif out.output_type == "error":
-        #             txt = [out.ename, out.evalue] + out.traceback
-        #             res.append(('error', txt))
-        #         elif out.output_type == "execute_result":
-        #             for mime_type, data in out.data.items():
-        #                 if mime_type == "text/plain":
-        #                     res.append(("execute_result", data))
-        #         elif out.output_type == "display_data":
-        #             for mime_type, data in out.data.items():
-        #                 if mime_type == "image/png":
-        #                     res.append(("display_data", data))
-        #
-        #     notebook_cells.append((cell.source, res))
+        notebook_cells = nbdef.cells
 
     view_params["notebook_cells"] = notebook_cells
 

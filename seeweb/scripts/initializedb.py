2262f7ba548f688c8616d701e88f432ba7283ca3
revesansparole <revesansparole@gmail.com>
2016-06-17 10:11:13 +0200

    better article view

--- seeweb/scripts/initializedb.py
+++ seeweb/scripts/initializedb.py
@@ -213,5 +213,30 @@ def main(argv=sys.argv):
                                revesansparole.id,
                                "test article")
         roa.doi = "10.1016/S0304-3800(98)00100-8"
+        descr = dedent("""
+            We present a new approach to simulate the distribution of natural
+            light within plant canopies. The canopy is described in 3D, each
+            organ being represented by a set of polygons. Our model calculates
+            the light incident on each polygon. The principle is to distinguish
+            for each polygon the contribution of the light coming directly from
+            light sources, the light scattered from close polygons and that
+            scattered from far polygons. Close polygons are defined as located
+            inside a sphere surrounding the studied polygon and having a
+            diameter Ds. The direct light is computed by projection. The
+            exchanges between close polygons are computed by the radiosity
+            method, whereas the contribution from far polygons is estimated by
+            a multi-layer model. The main part of computing time corresponds to
+            the calculations of the geometric coefficients of the radiosity
+            system. Then radiative exchanges can be quickly simulated for
+            various conditions of the angular distribution of incoming light
+            and various optical properties of soil and phytolelements.
+            Simulations compare satisfactorily with those produced by a Monte
+            Carlo ray tracing. They show that considering explicitly the close
+            neighboring of each polygon improves the estimation of organs
+            irradiance, by taking into account the local variability of fluxes.
+            For a virtual maize canopy, these estimations are satisfying with
+            Ds=0.5 m; in these conditions, the simulation time on a workstation
+            was 25 min for a canopy of 100 plants.""")
+        roa.store_description(descr)
         ROLink.connect(session, roc.id, roa.id, "contains")
         ROLink.connect(session, ro3.id, roa.id, "use")

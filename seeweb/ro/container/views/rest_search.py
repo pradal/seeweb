7cdd1775281d332fd5ec863a553f4f0dde3534cb
revesansparole <revesansparole@gmail.com>
2016-06-24 17:30:22 +0200

    adde auth check for rest search

--- seeweb/ro/container/views/rest_search.py
+++ seeweb/ro/container/views/rest_search.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 from seeweb.models import DBSession
+from seeweb.models.auth import Role
 from seeweb.models.ro_link import ROLink
 from seeweb.ro.container.models.ro_container import ROContainer
 
@@ -12,30 +13,37 @@ route_url = 'rest/ro_container/search'
 @view_config(route_name=route_name, renderer='json')
 def view(request):
     session = DBSession()
+    user = request.unauthenticated_userid
 
     if 'title' in request.GET:
         # search all RO whose title starts with something similar
         title = request.GET['title']
-        query = session.query(ROContainer).filter(ROContainer.title.like("%s%%" % title))
+        query = session.query(ROContainer)
+        query = query.filter(ROContainer.title.like("%s%%" % title))
         res = list(query.all())
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
         return dict(res=[ro.id for ro in res])
     elif 'contains' in request.GET:
         # search all RO that contains a specific id
         uid = request.GET['contains']
-        query = session.query(ROLink).filter(ROLink.target == uid, ROLink.type == 'contains')
-        res = [link.source for link in query.all()]
+        query = session.query(ROLink).filter(ROLink.target == uid,
+                                             ROLink.type == 'contains')
+        res = [ROContainer.get(session, link.source) for link in query.all()]
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
-        return dict(res=res)
+        return dict(res=[ro.id for ro in res])
     elif 'use' in request.GET:
         # search all RO that use a specific id
         uid = request.GET['use']
-        query = session.query(ROLink).filter(ROLink.target == uid, ROLink.type == 'use')
-        res = [link.source for link in query.all()]
+        query = session.query(ROLink).filter(ROLink.target == uid,
+                                             ROLink.type == 'use')
+        res = [ROContainer.get(session, link.source) for link in query.all()]
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
-        return dict(res=res)
+        return dict(res=[ro.id for ro in res])
     else:
         return {}
--- seeweb/views/ro/rest_search.py
+++ seeweb/views/ro/rest_search.py
@@ -2,6 +2,7 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 
 from seeweb.models import DBSession
+from seeweb.models.auth import Role
 from seeweb.models.research_object import ResearchObject
 from seeweb.models.ro_link import ROLink
 
@@ -12,10 +13,12 @@ def view(request):
     if 'type' in request.GET:
         ro_type = request.GET['type']
         print "ro_type", ro_type, repr(ro_type), "\n" * 10
-        loc = request.route_url('ro_%s_rest_search' % ro_type, _query=dict(request.GET))
+        loc = request.route_url('ro_%s_rest_search' % ro_type,
+                                _query=dict(request.GET))
         return HTTPFound(location=loc)
 
     session = DBSession()
+    user = request.unauthenticated_userid
 
     if 'uid' in request.GET:
         # search a RO with a specific id
@@ -25,32 +28,40 @@ def view(request):
             return {}
         else:
             # check credentials
-            pass
-
-            return ro.repr_json()
+            role = ro.access_role(session, user)
+            if role == Role.denied:
+                return {}
+            else:
+                return ro.repr_json()
     elif 'title' in request.GET:
         # search all RO whose title starts with something similar
         title = request.GET['title']
-        query = session.query(ResearchObject).filter(ResearchObject.title.like("%s%%" % title))
+        query = session.query(ResearchObject)
+        query = query.filter(ResearchObject.title.like("%s%%" % title))
         res = list(query.all())
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
         return dict(res=[ro.id for ro in res])
     elif 'contains' in request.GET:
         # search all RO that contains a specific id
         uid = request.GET['contains']
-        query = session.query(ROLink).filter(ROLink.target == uid, ROLink.type == 'contains')
-        res = [link.source for link in query.all()]
+        query = session.query(ROLink).filter(ROLink.target == uid,
+                                             ROLink.type == 'contains')
+        res = [ResearchObject.get(session, link.source) for link in query.all()]
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
-        return dict(res=res)
+        return dict(res=[ro.id for ro in res])
     elif 'use' in request.GET:
         # search all RO that use a specific id
         uid = request.GET['use']
-        query = session.query(ROLink).filter(ROLink.target == uid, ROLink.type == 'use')
-        res = [link.source for link in query.all()]
+        query = session.query(ROLink).filter(ROLink.target == uid,
+                                             ROLink.type == 'use')
+        res = [ResearchObject.get(session, link.source) for link in query.all()]
         # check credentials
+        res = [ro for ro in res if ro.access_role(session, user) != Role.denied]
 
-        return dict(res=res)
+        return dict(res=[ro.id for ro in res])
     else:
         return {}

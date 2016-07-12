23d5c1ffe4ce0d95394053562ad96ae03c734e5a
revesansparole <revesansparole@gmail.com>
2016-07-12 15:59:00 +0200

    updated init script

--- seeweb/scripts/pjt_workflow.py
+++ seeweb/scripts/pjt_workflow.py
@@ -3,6 +3,7 @@ from uuid import uuid1
 from seeweb.models.ro_link import ROLink
 from seeweb.ro.container.models.ro_container import ROContainer
 from seeweb.ro.interface.models.ro_interface import ROInterface
+from seeweb.ro.workflow_node.models.ro_workflow_node import ROWorkflowNode
 
 
 def main(session, user):
@@ -27,3 +28,9 @@ def main(session, user):
                                  iname)
 
         ROLink.connect(session, roc.id, roi.id, "contains")
+
+    rown = ROWorkflowNode.create(session,
+                                 uuid1().hex,
+                                 user.id,
+                                 "node")
+    ROLink.connect(session, roc.id, rown.id, "contains")

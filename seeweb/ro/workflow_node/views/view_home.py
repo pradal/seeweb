2048952799de1c44ef76c03caaa7f97b0648c835
revesansparole <revesansparole@gmail.com>
2016-07-13 14:49:09 +0200

    added some svg transfo for node

--- seeweb/ro/workflow_node/views/view_home.py
+++ seeweb/ro/workflow_node/views/view_home.py
@@ -1,7 +1,10 @@
+import json
 from jinja2 import Markup
+from openalea.wlformat.convert import svg
 from pyramid.view import view_config
 
 from seeweb.models import DBSession
+from seeweb.ro.interface.models.ro_interface import ROInterface
 from seeweb.views.ro.commons import view_init_min
 
 
@@ -16,5 +19,20 @@ def view(request):
     ro, view_params = view_init_min(request, session)
 
     view_params['description'] = Markup(ro.html_description())
+
+    store = {}
+    node_def = ro.load_definition()
+    for port in node_def['inputs'] + node_def['outputs']:
+        iid = port['interface']
+        iface = ROInterface.get(session, iid)
+        if iface is None:
+            pass
+        else:
+            store[iid] = iface.load_definition()
+            store[iid]['url'] = request.route_url('ro_view_home', uid=iid)
+
+    txt, viewbox = svg.export_node(node_def, store, (800, 300))
+    view_params['svg_repr'] = txt
+    view_params['svg_viewbox'] = json.dumps(viewbox)
 
     return view_params

603200d967c07caf68a19dc22ed19b04f612ce51
revesansparole <revesansparole@gmail.com>
2016-07-16 14:23:09 +0200

    auth pjt

--- seeweb/scripts/pjt_auth_managment.py
+++ seeweb/scripts/pjt_auth_managment.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 from seeweb.avatar import upload_user_avatar
+from seeweb.models.auth import Role
 from seeweb.models.ro_link import ROLink
 from seeweb.models.user import User
 
@@ -27,3 +28,29 @@ def main(session, user):
     img = Image.open("seeweb/scripts/avatar/sartzet.png")
     upload_user_avatar(img, other)
 
+    # user can view RO in container owner by other
+    roa = ROArticle()
+    roa.init(session, dict(creator=other.id, name="other article"))
+    roa.store_description("Title\n=====\n\nLorem Ipsum\nlorem ipsum")
+    roa.add_policy(session, user, Role.view)
+
+    road = ROArticle()
+    road.init(session, dict(creator=other.id, name="other editable article"))
+    road.store_description("Title\n=====\n\nLorem Ipsum\nlorem ipsum")
+    road.add_policy(session, user, Role.edit)
+
+    roc = ROContainer()
+    roc.init(session, dict(creator=other.id,
+                           name="other project",
+                           contents=[roa, road]))
+
+    # access granted to ROs through their container policy
+    roa = ROArticle()
+    roa.init(session, dict(creator=other.id, name="other 'private' article"))
+    roa.store_description("Title\n=====\n\nLorem Ipsum\nlorem ipsum")
+
+    roc = ROContainer()
+    roc.init(session, dict(creator=other.id,
+                           name="other project",
+                           contents=[roa]))
+    roc.add_policy(session, user, Role.view)

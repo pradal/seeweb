b0d4aac862dff89dc82b9447d6ae75b55de252dd
revesansparole <revesansparole@gmail.com>
2016-07-25 16:56:45 +0200

    added search and consume keyword

--- seeweb/ro/workflow_prov/search.py
+++ seeweb/ro/workflow_prov/search.py
@@ -1,3 +1,4 @@
+from seeweb.models.ro_link import ROLink
 from seeweb.ro.workflow_prov.models.ro_workflow_prov import ROWorkflowProv
 
 
@@ -17,5 +18,28 @@ def search(session, params):
         query = session.query(ROWorkflowProv.id)
         query = query.filter(ROWorkflowProv.name == name)
         return [uid for uid, in query.all()]
+    elif 'consume' in params:
+        # search all RO that have consumed the given data
+        uid = params['consume']
+        query = session.query(ROLink.source)
+        query = query.filter(ROLink.target == uid,
+                             ROLink.type == 'consume')
+        return [uid for uid, in query.all()]
+    elif 'produce' in params:
+        # search all RO that have been produced by an execution
+        uid = params['produce']
+        query = session.query(ROLink.target)
+        query = query.filter(ROLink.source == uid,
+                             ROLink.type == 'produce')
+        return [uid for uid, in query.all()]
+    elif 'use' in params:
+        # search all RO that use a specific id
+        uid = params['use']
+        query = session.query(ROLink.source)
+        query = query.filter(ROLink.target == uid,
+                             ROLink.type == 'use')
+        # TODO filter on workflow_prov
+        return [uid for uid, in query.all()
+                if ROWorkflowProv.get(session, uid) is not None]
     else:
         return []

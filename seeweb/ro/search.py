76f166c8da55edef9e80ba32305f3ea81815266d
revesansparole <revesansparole@gmail.com>
2016-08-04 16:06:14 +0200

    better search

--- seeweb/ro/search.py
+++ seeweb/ro/search.py
@@ -5,6 +5,7 @@ from os.path import basename, exists, splitext
 
 from seeweb.models import ro_search
 from seeweb.models.research_object import ResearchObject
+from seeweb.models.ro_link import ROLink
 
 
 # construct RO factory
@@ -39,7 +40,8 @@ def search(session, params):
     Returns:
         (list of str): list of ids of RO matching query
     """
-    query = session.query(ResearchObject)
+    # query on RO direct common attributes
+    query = session.query(ResearchObject.id)
     if "type" in params:
         query = query.filter(ResearchObject.type == params["type"])
 
@@ -54,10 +56,39 @@ def search(session, params):
         else:
             query = query.filter(ResearchObject.name == name)
 
-    query = query.order_by(ResearchObject.name)
+    # query = query.order_by(ResearchObject.name)
 
-    res = query.all()
+    uids = {uid for uid, in query.all()}
+
+    # query on type of link
+    if 'in' in params:
+        # search all RO in a specific container
+        uid = params['in']
+        query = session.query(ROLink.target)
+        query = query.filter(ROLink.source == uid, ROLink.type == 'contains')
+        uids &= {uid for uid, in query.all()}
+
+    if 'use' in params:
+        # search all RO that use a specific id
+        uid = params['use']
+        query = session.query(ROLink.source)
+        query = query.filter(ROLink.target == uid, ROLink.type == 'use')
+        uids &= {uid for uid, in query.all()}
+
+    # specific query by type
+    if "type" in params:
+        typ = params['type']
+        if typ in search_factory:
+            print "typed", typ, search_factory[typ], "\n" * 10
+
+    # fetch ROs
+    res = [ResearchObject.get(session, uid) for uid in uids]
+
+    # high level python expressed query
     if "toplevel" in params:
         res = [ro for ro in res if ro.is_lonely()]
 
-    return res
+    # sort by name
+    res = sorted([(ro.name, ro) for ro in res])
+
+    return [ro for name, ro in res]

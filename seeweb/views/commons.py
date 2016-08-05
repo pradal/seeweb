ff1feea27efa6544c0e443b953951bb50cbdd9bb
revesansparole <revesansparole@gmail.com>
2016-08-05 09:43:02 +0200

    main search actif from home page

--- seeweb/views/commons.py
+++ seeweb/views/commons.py
@@ -1,8 +1,5 @@
 from pyramid.httpexceptions import HTTPFound
 
-from seeweb.models.team import Team
-from seeweb.models.user import User
-
 
 def view_init(request, session):
     """Common init for all 'view'.
@@ -14,20 +11,10 @@ def view_init(request, session):
     Returns:
         dict of (str: any): view_params
     """
-    search_pattern = request.params.get("main_search", "")
-    if search_pattern != "":
-        user = User.get(session, search_pattern)
-        if user is not None:
-            loc = request.route_url('user_view_home', uid=user.id)
-            return HTTPFound(location=loc)
+    del session
 
-        team = Team.get(session, search_pattern)
-        if team is not None:
-            loc = request.route_url('team_view_home', uid=team.id)
-            return HTTPFound(location=loc)
-
-        loc = request.route_url('project_list',
-                                _query={"main_search": search_pattern})
-        return HTTPFound(location=loc)
+    if 'main_search' in request.params:
+        loc = request.route_url('ro_list', _query=dict(request.params))
+        raise HTTPFound(location=loc)
 
     return {}
